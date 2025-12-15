bfacf416fe581b479e5d63c6c69e3f018c105531
LazarintoDev <alumno.354114@ies-azarquiel.es>
2025-12-15 13:46:57 +0100

    2.2 Cargar el productCardComponent + estilos bootstrap

---
+++ src/app/components/product-card/product-card.ts
@@ -0,0 +1,20 @@
+import { Component, Input} from '@angular/core';
+import { Product } from '../../services/product';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {
+/* 
+  todo lo que me venga del input, lo almaceno en "producto", si no es null "!" 
+  productCard se lo pasaremos a la llamada en el hijo (product-list.html) como atributo de <app-product-card>
+  index igual a productCard, para indicar el numero de iteración del for del product-list.html
+*/
+  @Input() productCard!: Product;
+  @Input() index!: Number;
+
+}
--- src/app/components/products-list/products-list.ts
+++ src/app/components/products-list/products-list.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Product, ProductService } from '../../services/product';
+/* te traes el ProductCard */
+import { ProductCard } from '../product-card/product-card';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [],
+  imports: [ProductCard], /* no olvides aqui */
   templateUrl: './products-list.html',
   styleUrl: './products-list.css',
 })
 export class ProductsList {
 
-  /*   Variable para almacenar la lista de productos */
+  /*  Almecena la lista de productos */
   productos: Product[] = []
-  animarTabla = false
+  /*  Activa la clase fade-slide-in-list en el product-list.html */
+  animarLista = false
 
   constructor(private productService: ProductService) {
     this.productService.cargarProductos().subscribe(
@@ -20,7 +23,7 @@ export class ProductsList {
       (datos) => {
         setTimeout(() => {
           this.productos = datos
-          this.animarTabla = true /* da comienzo la animacion de la tabla */
+          this.animarLista = true /* da comienzo la animacion de las tarjetas-tabla */
           console.log('Productos cargados de la API', datos)
         }, 800)
 
